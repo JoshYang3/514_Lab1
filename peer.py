84cf4e289955a90c25016e909b57e101f49eab2d
JoshYang3 <joshyang0103@gamil.com>
2023-10-04 15:29:33 -0400

    peer can connect to server, server response w ACK

--- peer.py
+++ peer.py
@@ -7,7 +7,8 @@ def connect_to_server(server_ip, server_port):
     return client_socket
 
 def register_files(client_socket, files):
-    message = f'Register Request:{len(files)}|' + '|'.join(files)
+    file_info = '|'.join(f'{file["name"]},{file["size"]}' for file in files)
+    message = f'Register Request:{len(files)}|{file_info}'
     client_socket.send(message.encode('utf-8'))
     response = client_socket.recv(1024).decode('utf-8')
     print(response)
@@ -17,3 +18,33 @@ def request_file_list(client_socket):
     client_socket.send(message.encode('utf-8'))
     response = client_socket.recv(1024).decode('utf-8')
     print(response)
+
+def main():
+    # Create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    server_ip = '127.0.0.1'
+    server_port = 9999
+    client_socket.connect((server_ip, server_port))
+
+    # Create a list of files to register
+    files = [
+        {"name": "file1.txt", "size": 12345},
+        {"name": "file2.txt", "size": 67890}
+    ]
+
+    # Call the register_files function
+    register_files(client_socket, files)
+    request_file_list(client_socket)
+
+    # Close the socket when done
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
+    client_socket = connect_to_server("127.0.0.1", 9999)  # use the correct IP and port
+    files = [{"name": "test1", "size": 12345}]  # example files list
+    register_files(client_socket, files)
+    request_file_list(client_socket)
+    
--- server.py
+++ server.py
@@ -2,9 +2,30 @@
 
 import socket
 import threading
-import time
-import pickle as pk
 
-class Server(threading.Thread):
-    def __init__(self, max_connections, port):
-        self.host = 'localhost'
+def start_server(port=9999):
+    print("Starting server...")  # Add this line
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', port))
+    server_socket.listen(5)
+    print(f'Server listening on port {port}')
+
+    while True:
+        print("Waiting for a new connection...")  # Add this line
+        client_sock, addr = server_socket.accept()
+        print(f'Connection from {addr}')
+        client_handler = threading.Thread(target=handle_client, args=(client_sock,))
+        client_handler.start()
+        
+def handle_client(client_socket):
+    request = client_socket.recv(1024).decode('utf-8')
+    print(f'Received: {request}')
+    # Depending on the request, you might want to handle different types of messages
+    # For simplicity, sending an acknowledgement back
+    client_socket.send(b'ACK')
+    client_socket.close()
+
+if __name__ == "__main__":
+    start_server()
+    handle_client()
+
