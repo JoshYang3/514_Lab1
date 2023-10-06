1f7d258ede9693eaf2467f5c6dce90d365fd4394
JoshYang3 <joshyang0103@gamil.com>
2023-10-05 22:28:05 -0400

    peer can maintain the same connection until disconnect

--- peer.py
+++ peer.py
@@ -8,14 +8,27 @@ def connect_to_server(server_ip, server_port):
     return client_socket
 
 def register_files(client_socket, files):
-    file_info = '|'.join(f'{file["name"]},{file["size"]}' for file in files)
-    message = f'Register Request:{len(files)}|{file_info}'
+    file_info = '|'.join(f'{file["name"]}|{file["size"]}' for file in files)
+    message = f'Register Request|{len(files)}|{file_info}'
     client_socket.send(message.encode('utf-8'))
     response = client_socket.recv(1024).decode('utf-8')
     print(response)
 
 def request_file_list(client_socket):
     message = 'File List Request'
+    try:
+        client_socket.send(message.encode('utf-8'))
+        response = client_socket.recv(1024).decode('utf-8')
+        print(response)
+    except ConnectionResetError:
+        print("Connection was reset. The server might have closed the connection.")
+        return
+    except BrokenPipeError:
+        print("Broken pipe. The server might have closed the connection.")
+        return
+
+def disconnect(client_socket):
+    message = 'Disconnect'
     client_socket.send(message.encode('utf-8'))
     response = client_socket.recv(1024).decode('utf-8')
     print(response)
@@ -29,6 +42,12 @@ def receive_chunk(socket):
     chunk = socket.recv(chunk_size)
     return chunk
 
+def display_menu():
+    print("1. Register files")
+    print("2. Request file list")
+    print("3. Download file")
+    print("4. Exit")
+
 def main():
     # Create a socket object
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,7 +57,41 @@ def main():
     server_port = 9999
     client_socket.connect((server_ip, server_port))
 
-    # Create a list of files to register
+    while True:
+        display_menu()  # Display the menu at the start of each loop iteration
+        user_input = input("Enter the number of your choice: ")
+
+        if user_input == '1':
+            # ... (code to register files)
+            files = [
+                {"name": "file1.txt", "size": 12345},
+                {"name": "file2.txt", "size": 67890}
+            ]
+            register_files(client_socket, files)
+        
+        elif user_input == '2':
+            # ... (code to request file list)
+            request_file_list(client_socket)
+        
+        elif user_input == '3':
+            # ... (code to download file)
+            file_name = input("Enter the name of the file to download: ")
+            # Assume download_file is a function you've defined to handle file downloading
+            download_file(client_socket, file_name)
+        
+        elif user_input == '4':
+            print("Disconnecting...")
+            disconnect(client_socket)
+            break  # Exit the loop
+        
+        else:
+            print("Invalid choice, please try again.")
+
+    # Disconnect from the server
+    #print("Closing connection...")
+    #client_socket.close()
+
+    '''# Create a list of files to register
     files = [
         {"name": "file1.txt", "size": 12345},
         {"name": "file2.txt", "size": 67890}
@@ -54,12 +107,12 @@ def main():
     request_file_list(client_socket)
 
     # Close the socket when done
-    client_socket.close()
+    client_socket.close()'''
 
 if __name__ == "__main__":
     main()
-    client_socket = connect_to_server("127.0.0.1", 9999)  # use the correct IP and port
-    files = [{"name": "test1", "size": 12345}]  # example files list
-    register_files(client_socket, files)
-    request_file_list(client_socket)
+    #client_socket = connect_to_server("127.0.0.1", 9999)  # use the correct IP and port
+    #files = [{"name": "test1", "size": 12345}]  # example files list
+    #register_files(client_socket, files)
+    #request_file_list(client_socket)
     
--- server.py
+++ server.py
@@ -14,39 +14,52 @@ def start_server(port=9999):
         print("Waiting for a new connection...")  # Add this line
         client_sock, addr = server_socket.accept()
         print(f'Connection from {addr}')
-        client_handler = threading.Thread(target=handle_client, args=(client_sock,))
+        client_handler = threading.Thread(target=handle_client, args=(client_sock, addr))  # pass addr to the function
         client_handler.start()
         
-def handle_client(client_socket):
+'''def handle_client(client_socket):
     request = client_socket.recv(1024).decode('utf-8')
     print(f'Received: {request}')
     # Depending on the request, you might want to handle different types of messages
     # For simplicity, sending an acknowledgement back
     client_socket.send(b'ACK')
-    client_socket.close()
+    client_socket.close()'''
 
 file_registry = {}  # A dictionary to store file info and chunks
 
-def handle_client(client_socket):
-    request = client_socket.recv(1024).decode('utf-8')
-    print(f'Received: {request}')
+def handle_client(client_socket, client_addr):
+    try:
+        while True:
+            request = client_socket.recv(1024).decode('utf-8')
+            if not request:  # client might have disconnected
+                print(f"Client {client_addr} has disconnected.")
+                break
+            print(f'Received: {request}')
 
-    command, *args = request.split('|')
+            command, *args = request.split('|')
 
-    if command == 'Register Request':
-        # Process file registration
-        num_files = int(args[0])
-        files_info = args[1:]
-        for i in range(0, len(files_info), 2):
-            file_name, file_size = files_info[i:i+2]
-            file_registry[file_name] = {'size': int(file_size), 'chunks': []}
-        client_socket.send(b'ACK')
-
-    # ... (rest of your code)
-
-    client_socket.close()
+            if command == 'Register Request':
+                # Process file registration
+                #num_files = int(args[0])
+                files_info = args[1:]
+                for i in range(0, len(files_info), 2):
+                    file_name, file_size = files_info[i:i+2]
+                    file_registry[file_name] = {'size': int(file_size), 'chunks': []}
+                client_socket.send(b'ACK')
+            elif command == 'File List Request':
+                if(file_registry == {}):
+                    client_socket.send('File registry is empty'.encode('utf-8'))
+                else:
+                    file_list = '|'.join(file_registry.keys())
+                    client_socket.send(file_list.encode('utf-8'))
+            elif command == 'Disconnect':
+                print(f"Received disconnect command from client {client_addr}.")
+                break  # Exit the loop
+    except Exception as e:
+        print(f"Error occurred with client {client_addr}: {e}")
+    finally:
+        client_socket.close()
 
 if __name__ == "__main__":
     start_server()
-    handle_client()
 
